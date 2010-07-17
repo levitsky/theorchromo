efa1fc4bd72ced8133ffbd03a422f52c75d709c3
Anton Goloborodko <goloborodko.anton@gmail.com>
2010-07-17 18:17:36 +0400

    Bug fixes in the settings.py

--- theorchromo_online/settings.py
+++ theorchromo_online/settings.py
@@ -3,7 +3,7 @@ import os
 # Django settings for theorchromo_online project.
 
 import socket
-if socket.get_hostname() == 'bezalel-6720s':
+if socket.gethostname() == 'bezalel-6720s':
     DEBUG = True
 else:
     DEBUG = False
