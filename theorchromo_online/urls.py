0f1113a0de930f10834856f6a8af06340703368d
Lev Levitsky <lev.levitsky@phystech.edu>
2013-12-01 16:10:26 +0400

    Fix imports for Django 1.6

--- theorchromo_online/urls.py
+++ theorchromo_online/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 from theorchromo_online.views import *
 import settings
 
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Example:
     # (r'^theorchromo_online/', include('theorchromo_online.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
